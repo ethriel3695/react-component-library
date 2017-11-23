787a947d485fb27b7517a17c1ad8fc5554b9b66c
Reuben Ellis <ethriel3695@gmail.com>
2017-11-23 07:47:08 -0700

    Added metadata generation and an example component

---
+++ src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/** A super awesome component which says Hello with a custom message. */
+const HelloWorld = ({message}) => {
+  return <div>Hello {message}</div>
+};
+
+HelloWorld.propTypes = {
+  message: PropTypes.string
+};
+
+HelloWorld.defaultProps = {
+  message: 'World'
+};
+
+export default HelloWorld;
---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from 'react-component-library/HelloWorld';
+
+/** Custom message */
+export default function ExampleHelloWorld() {
+  return <HelloWorld message='Pluralsight viewers!' />
+}
