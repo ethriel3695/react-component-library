8be29b1d59e9a6055cec37fc9cbc6e4fff6e95cb
Reuben Ellis <ethriel3695@gmail.com>
2017-11-24 08:22:01 -0700

    Added atoms and molecules

---
+++ src/docs/examples/Label/ExampleRequired.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Label from 'react-component-library/Label';
+
+/** Required label */
+export default function ExampleRequired() {
+  return <Label htmlFor='test' label='test' required />
+}
